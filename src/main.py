2d366b10eeb07626c4b18197d587d6fb9414f296
salgado2004 <leosalgado2004@gmail.com>
2024-12-01 21:20:02 -0300

    Filtro sobel

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 import cv2
 
+def sobel(img):
+    img_gray = img
+    grad_x = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
+    grad_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
+    abs_grad_x = cv2.convertScaleAbs(grad_x)
+    abs_grad_y = cv2.convertScaleAbs(grad_y)
+    img_sobel = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    return img_sobel
+
 for i in range(6):
     # Ler a imagem
     img = cv2.imread(f"assets/img-{i+1}.jpg")
@@ -7,7 +16,6 @@ for i in range(6):
     B, G, R = cv2.split(img)
 
     # Exibir a imagem
-    cv2.imshow("Normal", img)
-    cv2.imshow("Cinza", gray)
     cv2.imshow("Red", R)
+    cv2.imshow("Sobel", sobel(R))
     cv2.waitKey(0)
